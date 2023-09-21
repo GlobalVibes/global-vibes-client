e4eac6b59c3c6b82edd2bc4aea86ae8ab038bd51
Priscila Borges <priscila.borges.nl@gmail.com>
2023-09-21 15:34:19 +0200

    Add component addpost to user home page

--- src/assets/pages/UserHomePage.jsx
+++ src/assets/pages/UserHomePage.jsx
@@ -1,10 +1,12 @@
 import UserDetails from "../components/UserDetails";
 import UserPosts from "../components/UserPosts";
+import AddPost from "../components/AddPost"
 
 function UserHomePage() {
     return (
         <div>
             <UserDetails/>
+            <AddPost />
             <UserPosts />
         </div>
     )
