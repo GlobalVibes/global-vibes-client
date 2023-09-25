a197e73101431636830945ca3a8a8a21d621c7f0
Priscila Borges <priscila.borges.nl@gmail.com>
2023-09-25 15:09:40 +0200

    implement delete post

--- src/assets/components/UserPosts.jsx
+++ src/assets/components/UserPosts.jsx
@@ -1,47 +1,49 @@
 import axios from "axios";
 import { useContext, useState, useEffect } from "react";
-const storedToken = localStorage.getItem("authToken");
 import { AuthContext } from '../context/auth.contex';
+import { useNavigate } from "react-router-dom";
+const storedToken = localStorage.getItem("authToken");
 
 function UserPosts() {
 
     const { user } = useContext(AuthContext);
     const [userPosts, setUserPosts] = useState([])
-    
+
+
+    const navigate = useNavigate()
 
     useEffect(() => {
-       
-        
+
         axios
-        .get(`${import.meta.env.VITE_API_URL}/api/posts`)
-        .then(response => {
-            console.log(response.data)
-            setUserPosts(response.data)
-        })
-        .catch((error) => console.log(error));
+            .get(`${import.meta.env.VITE_API_URL}/api/posts`)
+            .then(response => {
+                setUserPosts(response.data)
+            })
+            .catch((error) => console.log(error));
     }, []);
 
     const handleDelete = (postId) => {
+
         axios
-            .delete(`${import.meta.env.VITE_API_URL}/api/posts`, { headers: { Authorization: `Bearer ${storedToken}` } })
-            .then((response) => {
-                setUserPosts(userPosts.filter((post) => post.id !== postId));
+            .delete(`${import.meta.env.VITE_API_URL}/api/posts/${postId}`, { headers: { Authorization: `Bearer ${storedToken}` } })
+            .then(() => {
+
+                setUserPosts(userPosts.filter((current) => current._id !== postId))
+
             })
             .catch((error) => console.log(error));
     };
-    const handleUpdate = (postId) => {
-        // will be implemented later
-    };
+
     return (
         <div className="user-posts">
             <h2>Your posts</h2>
-            { userPosts && userPosts.map((post) => (
-                <div key={post.id} className="post">
+            {userPosts && userPosts.map((post) => (
+                <div key={post._id} className="post">
                     <img src={post.image} alt={post.description} />
                     <p>{post.description}</p>
                     <p>{post.hobbie}</p>
-                    <button onClick={() => handleDelete(post.id)}>Delete</button>
-                    <button onClick={() => handleUpdate(post.id)}>Update</button>
+                    <button onClick={(e) => handleDelete(post._id)}>Delete</button>
+                    <button onClick={() => handleUpdate(post._id)}>Update</button>
                 </div>
             ))}
         </div>
