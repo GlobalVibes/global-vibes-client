f41cc41f03fc04f606947ad0c5223e4842479520
RobertoZ182 <rkyuchukov18@gmail.com>
2023-09-25 16:13:27 +0300

    merge conflict

--- src/assets/components/AddPost.jsx
+++ src/assets/components/AddPost.jsx
@@ -1,101 +1,95 @@
 import axios from "axios";
 import { useState, useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { AuthContext } from '../context/auth.contex';
+import { AuthContext } from "../context/auth.contex";
+
 function AddPost() {
+  const { storedToken } = useContext(AuthContext);
+  const [hobbies, setHobbies] = useState([]);
+  const [newPost, setNewPost] = useState({
+    image: "",
+    description: "",
+    hobby: "",
+  });
 
-    const navigate = useNavigate();
-    const { storedToken } = useContext(AuthContext);
-    const [hobbies, setHobbies] = useState([]);
-    const [newPost, setNewPost] = useState({
-        image: "",
-        description: "",
-        hobby: "",
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewPost({
+      ...newPost,
+      [name]: value,
     });
+  };
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setNewPost({
-            ...newPost,
-            [name]: value,
-        });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newPost);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(newPost);
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/api/posts`, newPost, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => {
+        setNewPost(" ");
+      })
+      .catch((error) => {
+        console.error("Error creating a new post:", error);
+      });
+  };
 
-        axios
-            .post(`${import.meta.env.VITE_API_URL}/api/posts`, newPost, { headers: { Authorization: `Bearer ${storedToken}` } })
-            .then((response) => {
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/api/hobbies`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((hobbies) => {
+        setHobbies(hobbies.data);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  }, []);
 
-                const createdPost = response.data;
-                navigate(`/user-homepage`);
-            })
-            .catch((error) => {
-                console.error("Error creating a new post:", error);
-            });
-    };
+  return (
+    newPost && (
+      <div>
+        <h2>Add a New Post</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="image">Image URL:</label>
+            <input
+              type="text"
+              id="image"
+              name="image"
+              value={newPost.image}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description:</label>
+            <textarea
+              id="description"
+              name="description"
+              value={newPost.description}
+              onChange={handleInputChange}
+              required
+            ></textarea>
+          </div>
+          <div>
+            <label htmlFor="hobby">Hobby:</label>
 
-    useEffect(() => {
-        axios
-            .get(`${import.meta.env.VITE_API_URL}/api/hobbies`, {
-                headers: { Authorization: `Bearer ${storedToken}` },
-            })
-            .then((hobbies) => {
-                setHobbies(hobbies.data);
-            })
-            .catch((error) => {
-                console.log("Error: ", error);
-            });
-    }, []);
-
-    return (
-        newPost && (
-        <div>
-            <h2>Add a New Post</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="image">Image URL:</label>
-                    <input
-                        type="text"
-                        id="image"
-                        name="image"
-                        value={newPost.image}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="description">Description:</label>
-                    <textarea
-                        id="description"
-                        name="description"
-                        value={newPost.description}
-                        onChange={handleInputChange}
-                        required
-                    ></textarea>
-                </div>
-                <div>
-                    <label htmlFor="hobby">Hobby:</label>
-                   
-                    <select id="hobby">
-                        {hobbies.map((elem) => {
-                            return (
-                                <option key={elem._id} value={elem.description}>{elem.title}</option>
-                            )
-                        })}
-                    </select>
-                </div>
-                <button type="submit">Add Post</button>
-            </form>
-        </div>
-        )
-    );
+            <select id="hobby">
+              {hobbies.map((elem) => {
+                return (
+                  <option key={elem._id} value={elem.description}>
+                    {elem.title}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <button type="submit">Add Post</button>
+        </form>
+      </div>
+    )
+  );
 }
 export default AddPost;
-
-
-
-
-
-
