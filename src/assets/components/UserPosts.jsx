6c4090ddce97817a78d1a798e1f171ea43ec6728
Priscila Borges <priscila.borges.nl@gmail.com>
2023-09-27 07:43:27 +0200

    correct a typo in hobby

--- src/assets/components/UserPosts.jsx
+++ src/assets/components/UserPosts.jsx
@@ -39,7 +39,7 @@ function UserPosts() {
                 <div key={post._id} className="post">
                     <img src={post.image} alt={post.description} />
                     <p>{post.description}</p>
-                    <p>{post.hobbie}</p>
+                    <p>{post.hobby}</p>
                     <button onClick={(e) => handleDelete(post._id)}>Delete</button>
                     <Link to={`/update/${post._id}`}>Update</Link>
                 </div>
