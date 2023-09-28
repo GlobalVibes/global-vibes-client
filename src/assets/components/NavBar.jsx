5464c446a026e6a58921b5b33437384f4b0021f2
Priscila Borges <priscila.borges.nl@gmail.com>
2023-09-28 19:08:46 +0200

    update Navbar buttons

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -6,23 +6,23 @@ import GlobalVibes from '../pages/images/GlobalVibesLogo.png';
 function NavBar() {
 
   const {
-    isLoggedIn,    
+    isLoggedIn,
     logOutUser
-  } = useContext(AuthContext); 
+  } = useContext(AuthContext);
 
   return (
     <div id="navbar">
       {isLoggedIn && (
-        <div style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center'}}>
-          <img src={GlobalVibes} alt="Logo" width='200' height='150' style={{objectFit: 'cover'}}/>
-          <div style={{display: 'flex', flexDirection: 'row-reverse', justifyContent:'center', alignItems:'center', gap: '20px', padding: '30px'}}>
-          <Link to="/user-homepage">
-            <button id="homepagebutton">Homepage</button>
-          </Link>
-          <Link to="/">
-            <button onClick={logOutUser} id="logoutbutton">Logout</button>
-          </Link>          
-          <Link to="/allposts"> <button id="checkotherpostsbutton">Check other posts</button> </Link>
+        <div style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center' }}>
+          <img src={GlobalVibes} alt="Logo" width='200' height='150' style={{ objectFit: 'cover' }} />
+          <div style={{ display: 'flex', flexDirection: 'row-reverse', justifyContent: 'center', alignItems: 'center', gap: '20px', padding: '30px' }}>
+            <Link to="/">
+              <button onClick={logOutUser} id="logoutbutton">Logout</button>
+            </Link>            
+            <Link to="/allposts"> <button id="checkotherpostsbutton">Check other posts</button> </Link>
+            <Link to="/user-homepage">
+              <button id="homepagebutton">Homepage</button>
+            </Link>
           </div>
         </div>
 
