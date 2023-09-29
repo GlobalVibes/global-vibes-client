9f594cbcfb166553002a88abfdb08e473344cc9c
RobertoZ182 <rkyuchukov18@gmail.com>
2023-09-29 10:47:46 +0300

    Styling HomePage

--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -6,16 +6,14 @@ function HomePage(){
         <div id="upperPage">
           <img src={Logo} alt="Logo" />
           <div id="btn-container">
-            <button><Link to="/login" style={{color: 'white', textDecoration: 'none'}}>Log in</Link></button>
-            <button><Link to="/signup" style={{color: 'white', textDecoration: 'none'}}>Sign up</Link></button>
+            <button class="btn"><Link to="/login" style={{color: 'white', textDecoration: 'none'}}>Log in</Link></button>
+            <button class="btn"><Link to="/signup" style={{color: 'white', textDecoration: 'none'}}>Sign up</Link></button>
+            <Link to="/posts" style={{color: 'white', textDecoration: 'none'}}><button id="checkourpostbutton">Check our posts</button></Link>
           </div>
         </div>
         <div id="lowerPart">
-          <p>Welcome! The biggest platform for global meetup</p>
-          <Link to="/posts" style={{color: 'white', textDecoration: 'none'}}>Check our posts</Link>
+          <h2 id="welcometext">Welcome! The biggest platform for global meetup</h2>          
         </div>
-        
-
       </div>
     );
 }
